ec3065c5612976d0e891f607910253d14aea0fbd
Romano Livi <romano_livi@outlook.com>
2020-12-26 17:09:48 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -7,7 +7,7 @@ class Song
   
   def initialize(name, artist = nil, genre = nil)
     @name = name 
-    self.artist=artist unless artist == nil
+    self.artist = artist unless artist == nil
     self.genre = genre unless genre == nil 
   end
   
